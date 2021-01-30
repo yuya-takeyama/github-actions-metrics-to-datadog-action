ec8d9f36f695a509f6058b671ac8b88aa2d37dad
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-01-30 11:34:47 +0900

    Fix setFailed

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { getInputs } from './inputs';
 import { sendMetrics } from './sendMetrics';
-import core from '@actions/core';
+import { setFailed } from '@actions/core';
 
 (async () => {
   try {
     await sendMetrics(getInputs());
   } catch (err) {
-    core.setFailed(err);
+    setFailed(err);
   }
 })();
